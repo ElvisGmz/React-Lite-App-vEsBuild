8c167e3ce1b05d073df0927637858e53edb6e175
Elvis Gómez <38594674+ElvisGmz@users.noreply.github.com>
2021-02-22 02:42:23 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ export default function App() {
         </div>
         <a href="https://github.com/ElvisGmz" target="_blank">
           <img
-            src="https://www.flaticon.com/svg/vstatic/svg/25/25471.svg?token=exp=1613981896~hmac=f0bf34872ac5c5d764e7cd11ba978c79"
+            src="https://image.flaticon.com/icons/png/512/25/25231.png"
             alt=""
           />
-          <br />
         </a>
+          <br />
         <a href="https://ko-fi.com/elvisgmz" target="_blank">
           <img src="https://ko-fi.com/img/githubbutton_sm.svg" alt="" />
         </a>
